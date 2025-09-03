a81c0e81d97ea1bb052f7af721e677e685be056b
Cory Buecker <email@corybuecker.com>
2025-09-03 05:57:20 -0500

    feat(pico-2w-i2c-usb,read-usb): add new USB CDC example and host reader projects
    - Add new  project for USB CDC device on RP235x
    - Add new  Rust host project to read from CDC device
    - Add VSCode configs, linker scripts, and dependencies for both
    - Update  config for GPIOTE and logging
    - Remove old  project and all its files
    
    This commit introduces a new USB CDC device example for the Raspberry Pi Pico 2W (RP235x) using Embassy, and a host-side Rust project to read from the device. The old  project is removed in favor of the new implementation.

---
+++ pico-2w-i2c-usb/src/main.rs
@@ -0,0 +1,40 @@
+#![no_std]
+#![no_main]
+
+mod common;
+mod usb;
+
+use defmt::{info, unwrap, warn};
+use embassy_executor::Spawner;
+use embassy_time::Timer;
+use embassy_usb::driver::EndpointError;
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+
+    let mut usb = usb::create_usb_device(p.USB);
+    unwrap!(spawner.spawn(usb::usb_task(usb.usb_device)));
+
+    loop {
+        info!("Waiting for USB Connection");
+        usb.class.wait_connection().await;
+        info!("USB Connected");
+
+        let message = b"Hello from Rust!\r\n";
+        loop {
+            match usb.class.write_packet(message).await {
+                Ok(_) => {}
+                Err(EndpointError::Disabled) => {
+                    info!("USB Disconnected");
+                    break;
+                }
+                Err(e) => {
+                    warn!("Write error: {:?}", e);
+                }
+            }
+
+            Timer::after_millis(1000).await;
+        }
+    }
+}
